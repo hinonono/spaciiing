3d033dc4aeba963ba28ab9dc5bd4a92161fd9d4f
Hsieh <johnny.hsieh.12@gmail.com>
2022-07-16 16:17:46 +0800

    create ui

--- spaciiing/code.ts
+++ spaciiing/code.ts
@@ -1,4 +1,6 @@
-figma.showUI(__html__);
+figma.showUI(__html__, { themeColors: true, /* other options */ })
+figma.ui.resize(320, 320);
+
 figma.ui.onmessage = message => {
   figma.closePlugin();
 };
