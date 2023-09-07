c4b81fca07631f02c81c10df8443b6599a174726
chengyi <johnny.hsieh.12@gmail.com>
2023-09-07 20:57:16 +0800

    放棄治療，直接把CY Toolbox抄過來，然後把不要的功能隱蔽掉

--- spaciiing/src/code.ts
+++ spaciiing/src/code.ts
@@ -5,7 +5,7 @@ import * as GEN from "./generate-module";
 import * as LOREM from "./lorem-module";
 
 figma.showUI(__html__, { themeColors: true });
-figma.ui.resize(280, 700);
+figma.ui.resize(300, 400);
 
 figma.ui.onmessage = (message) => {
   console.log(message);
