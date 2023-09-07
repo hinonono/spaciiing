61cc55451cc161d6282644b2ce9bc6cffe71b9a9
chengyi <johnny.hsieh.12@gmail.com>
2023-09-07 22:13:44 +0800

    新增日本語

--- spaciiing/src/localizations/localizations.ts
+++ spaciiing/src/localizations/localizations.ts
@@ -1,8 +1,10 @@
 import { Localizations } from "../fundamental-module";
 import { en } from "./en";
+import { jp } from "./jp";
 import { zh_hant } from "./zh_Hant";
 
 export const localizations: Localizations = {
   en: en,
   zh_Hant: zh_hant,
+  jp: jp
 };
