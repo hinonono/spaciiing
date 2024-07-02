d2be9c601a45b3583d9e7f5674a4112f9b199b5a
chengyi <johnny.hsieh.12@gmail.com>
2024-07-02 19:16:00 +0800

    全體翻譯優化

--- src/i18n.ts
+++ src/i18n.ts
@@ -24,30 +24,39 @@ import module_ja_JP from "../public/locales/ja-JP/module.json";
 import module_zh_TW from "../public/locales/zh-TW/module.json";
 import module_zh_CN from "../public/locales/zh-CN/module.json";
 
+import term_en_US from "../public/locales/en-US/term.json";
+import term_ja_JP from "../public/locales/ja-JP/term.json";
+import term_zh_TW from "../public/locales/zh-TW/term.json";
+import term_zh_CN from "../public/locales/zh-CN/term.json";
+
 const resources = {
   enUS: {
     common: common_en_US,
     settings: settings_en_US,
     license: license_en_US,
     module: module_en_US,
+    term: term_en_US,
   },
   jaJP: {
     common: common_ja_JP,
     settings: settings_ja_JP,
     license: license_ja_JP,
     module: module_ja_JP,
+    term: term_ja_JP,
   },
   zhTW: {
     common: common_zh_TW,
     settings: settings_zh_TW,
     license: license_zh_TW,
     module: module_zh_TW,
+    term: term_zh_TW,
   },
   zhCN: {
     common: common_zh_CN,
     settings: settings_zh_CN,
     license: license_zh_CN,
     module: module_zh_CN,
+    term: term_zh_CN,
   },
 };
 
