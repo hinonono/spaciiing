a7d061f72c40faeb344e3344975b5870f6a834de
chengyi <johnny.hsieh.12@gmail.com>
2024-08-14 16:34:29 +0800

    優化tab bar，現在可以drag和左右滑動

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Module } from "../types/Module";
 import {
   SvgDefaultStyleLibrary,
@@ -37,8 +37,70 @@ interface TabBarProps {
 
 const TabBar: React.FC<TabBarProps> = ({ activeTab, setActiveTab }) => {
   const { licenseManagement } = useAppContext();
-  licenseManagement.licenseKey;
-  licenseManagement.tier;
+
+  // Reference to the scrollable container element
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+  // State to track if the user is currently dragging for horizontal scroll
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+
+  // State to store the initial X position when the drag starts
+  const [startX, setStartX] = useState<number>(0);
+
+  // State to store the initial scroll position when the drag starts
+  const [scrollLeft, setScrollLeft] = useState<number>(0);
+
+  // Function to handle the mouse down event for initiating the drag
+  const onMouseDown = (e: React.MouseEvent) => {
+    setIsDragging(true);
+    if (scrollContainerRef.current) {
+      // Calculate the initial X position relative to the scroll container
+      setStartX(e.pageX - scrollContainerRef.current.offsetLeft);
+      // Store the current scroll position
+      setScrollLeft(scrollContainerRef.current.scrollLeft);
+    }
+  };
+
+  const onMouseLeave = () => {
+    setIsDragging(false);
+  };
+
+  const onMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  // Function to handle mouse move event for horizontal scrolling
+  const onMouseMove = (e: React.MouseEvent) => {
+    if (!isDragging || !scrollContainerRef.current) return;
+
+    // Calculate the new scroll position
+    const x = e.pageX - scrollContainerRef.current.offsetLeft;
+    const walk = (x - startX) * 2; // The multiplier controls the scrolling speed
+    scrollContainerRef.current.scrollLeft = scrollLeft - walk;
+  };
+
+  const onWheel = (e: React.WheelEvent) => {
+    if (scrollContainerRef.current) {
+      e.preventDefault(); // Prevent the default vertical scroll behavior
+      scrollContainerRef.current.scrollLeft += e.deltaY; // Scroll horizontally
+    }
+  };
+
+  useEffect(() => {
+    const scrollContainer = scrollContainerRef.current;
+    if (scrollContainer) {
+      scrollContainer.addEventListener("wheel", (e: WheelEvent) =>
+        onWheel(e as unknown as React.WheelEvent)
+      );
+    }
+    return () => {
+      if (scrollContainer) {
+        scrollContainer.removeEventListener("wheel", (e: WheelEvent) =>
+          onWheel(e as unknown as React.WheelEvent)
+        );
+      }
+    };
+  }, []);
 
   const renderTabContent = () => {
     switch (activeTab) {
@@ -72,7 +134,14 @@ const TabBar: React.FC<TabBarProps> = ({ activeTab, setActiveTab }) => {
   return (
     <div className="tabs">
       <SaleBannerWrapper licenseManagement={licenseManagement} />
-      <div className="tab-bar">
+      <div
+        className="tab-bar scroll-container hide-scrollbar-horizontal"
+        ref={scrollContainerRef}
+        onMouseDown={onMouseDown}
+        onMouseLeave={onMouseLeave}
+        onMouseUp={onMouseUp}
+        onMouseMove={onMouseMove}
+      >
         <TabButton
           activeTab={activeTab}
           tabName="Spaciiing"
@@ -127,12 +196,12 @@ const TabBar: React.FC<TabBarProps> = ({ activeTab, setActiveTab }) => {
           setActiveTab={setActiveTab}
           SvgComponent={SvgDefaultStyleLibrary}
         />
-        {/* <TabButton
+        <TabButton
           activeTab={activeTab}
           tabName="StyleIntroducer"
           setActiveTab={setActiveTab}
           SvgComponent={SvgCatalogue}
-        /> */}
+        />
         <TabButton
           activeTab={activeTab}
           tabName="PluginSetting"
