c75b418e7fb157f0e71a35b6bf28da9e983fb9f5
chengyi <johnny.hsieh.12@gmail.com>
2024-10-21 09:39:01 +0800

    開始重新製作Memorizer

--- src/view/PropertyClipboard.tsx
+++ src/view/PropertyClipboard.tsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useAppContext } from "../AppProvider";
+import { TitleBar } from "../components";
+import Modal from "../components/Modal";
+import { checkProFeatureAccessibleForUser } from "../module-frontend/utilFrontEnd";
 
-interface PropertyClipboardProps {
-  
-}
+interface PropertyClipboardProps {}
 
 const PropertyClipboard: React.FC<PropertyClipboardProps> = () => {
+  // 多語言化
+  const { t } = useTranslation(["module"]);
+
+  // 功能說明彈窗
+  const { licenseManagement, setShowCTSubscribe } = useAppContext();
+  const [showExplanationModal, setShowExplanationModal] = useState(false);
+  const handleOpenExplanationModal = () => setShowExplanationModal(true);
+  const handleCloseExplanationModal = () => setShowExplanationModal(false);
+
+  // 記憶所選取的物件作為參考目標
+  const memorizeSelectedObject = () => {
+    if (!checkProFeatureAccessibleForUser(licenseManagement)) {
+      setShowCTSubscribe(true);
+      return;
+    }
+  };
+
+  // 貼上指定的屬性至所選擇的物件
+  const pastePropertyToSelectedObject = () => {
+    if (!checkProFeatureAccessibleForUser(licenseManagement)) {
+      setShowCTSubscribe(true);
+      return;
+    }
+  };
 
   return (
-    <div></div>
+    <div>
+      <Modal
+        show={showExplanationModal}
+        handleClose={handleCloseExplanationModal}
+      >
+        <div>
+          <h3>{t("module:modulePropertyClipboard")}</h3>
+          <p>{t("module:modulePropertyClipboardDesc")}</p>
+        </div>
+      </Modal>
+      <TitleBar
+        title={t("module:modulePropertyClipboard")}
+        onClick={handleOpenExplanationModal}
+        isProFeature={true}
+      />
+      <div className="content"></div>
+    </div>
   );
 };
 
