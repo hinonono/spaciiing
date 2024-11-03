2421064de60d98c950f870f130253f5d41e68f97
chengyi <johnny.hsieh.12@gmail.com>
2024-11-03 18:41:29 +0800

    改善免費試用彈窗的資訊揭露

--- src/components/NormalBanner.tsx
+++ src/components/NormalBanner.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useAppContext } from "../AppProvider";
+import info from "../assets/info.json";
 
 interface NormalBannerProps {}
 
@@ -11,7 +12,13 @@ const NormalBanner: React.FC<NormalBannerProps> = () => {
   return (
     <div className="banner flex flex-justify-spacebetween align-items-center">
       <div className="frame-group">
-        <div className="message-primary">{t("license:tryAllFeaturesSevenDaysFree")}</div>
+        <div className="message-primary">{t("license:trySevenDaysFree")}</div>
+        <div className="note message-secondary mt-xxxsmall">
+          {t("license:monthlyPriceDesc").replace(
+            "$MONTHLY_PRICE$",
+            "$" + info.price.monthly
+          )}
+        </div>
       </div>
       <button
         className="button button--special"
