e816fe81ea4ca08fd71d01482f62f0d3867583a6
chengyi <johnny.hsieh.12@gmail.com>
2024-11-05 08:23:33 +0800

    移除免費試用彈窗中殘留的 輸入驗證金鑰彈窗 code

--- src/view/SubscriptionModal.tsx
+++ src/view/SubscriptionModal.tsx
@@ -1,123 +1,15 @@
 import React, { useState } from "react";
-import Modal from "../components/Modal";
-import useNetworkStatus from "../useNetworkStatus";
-import { FigmaButton, SectionTitle } from "../components";
-import * as paymentsUtil from "../module-frontend/paymentsUtil";
-import * as util from "../module/util";
-import {
-  LicenseManagement,
-  LicenseResponse,
-  LicenseResponseSuccess,
-} from "../types/LicenseManagement";
-import { useAppContext } from "../AppProvider";
-import axios from "axios";
-import { useTranslation } from "react-i18next";
-import { handleSubscriptionStatus } from "../module-frontend/licenseManagementFrontEnd";
-import { MessageLicenseManagement } from "../types/Messages/MessageLicenseManagement";
-import NewFreeTrialView from "../components/temp/freeTrialView/NewFreeTrialView";
 import info from "../info.json";
+import Modal from "../components/Modal";
+import { useAppContext } from "../AppProvider";
+import NewFreeTrialView from "../components/temp/freeTrialView/NewFreeTrialView";
 import LegacyFreeTrialView from "../components/temp/freeTrialView/LegacyFreeTrialView";
 
 const SubscriptionModal: React.FC = () => {
-  const { licenseManagement } = useAppContext();
-  const { t } = useTranslation(["license", "term"]);
-
-  const isOnline = useNetworkStatus();
-  // 金鑰輸入
-  const [licenseKey, setLicenseKey] = useState("");
-  // API 呼叫
-  const [response, setResponse] = useState<LicenseResponse | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState(false);
-  const {
-    showCTSubscribe,
-    showActivateModal,
-    setShowCTSubscribe,
-    setShowActivateModal,
-    setLicenseManagement,
-  } = useAppContext();
-
-  const handleVerify = async () => {
-    if (licenseKey == "") {
-      setError(t("license:licenseKeyCannotBeNull"));
-      return;
-    }
-    setLoading(true);
-    setError(null);
-    try {
-      const response: LicenseResponse = await paymentsUtil.verifyLicenseKey(
-        licenseKey
-      );
-
-      if (response.success === true) {
-        let newLicense: LicenseManagement = {
-          tier: "UNKNOWN",
-          recurrence: "",
-          isLicenseActive: false,
-          licenseKey: licenseKey,
-          sessionExpiredAt: util.addHours(new Date(), 3).toUTCString(),
-        };
-
-        newLicense = handleSubscriptionStatus(
-          response as LicenseResponseSuccess,
-          newLicense
-        );
-
-        const message: MessageLicenseManagement = {
-          license: newLicense,
-          module: "LicenseManagement",
-          phase: "Actual",
-          direction: "Inner",
-          action: "UPDATE",
-        };
-        parent.postMessage(
-          {
-            pluginMessage: message,
-          },
-          "*"
-        );
-        setLicenseManagement(newLicense);
-
-        console.log("License is successfully activated.");
-      }
-
-      setResponse(response);
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        console.log("🥹");
-
-        // Access the response data if the error is from Axios
-        setError(error.response ? error.response.data.message : error.message);
-      } else if (error instanceof Error) {
-        // Handle other types of errors
-        console.log("💖");
-        console.error(error.message);
-        setError(t("license:activateInvalid"));
-      } else {
-        console.log("🍏");
-        setError(t("license:activateUnknown"));
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
+  const { showCTSubscribe, setShowCTSubscribe } = useAppContext();
 
   const handleCloseCTSubscribe = () => {
     setShowCTSubscribe(false);
-    setShowActivateModal(false);
-    setError(null);
-    setLicenseKey("");
-    setResponse(null);
-  };
-  const handleOpenActivateModal = () => {
-    setShowCTSubscribe(true);
-    setShowActivateModal(true);
-  };
-  const handleCloseActivateModal = () => {
-    setShowActivateModal(false);
-    setLicenseKey("");
-    setError(null);
-    setResponse(null);
   };
 
   const freeTrialViewHandler = () => {
@@ -130,66 +22,7 @@ const SubscriptionModal: React.FC = () => {
 
   return (
     <Modal show={showCTSubscribe} handleClose={handleCloseCTSubscribe}>
-      <div>
-        {/* {showActivateModal && (
-          <div>
-            <h3>{t("license:enterLicenseKey")}</h3>
-            <p>{t("license:enterLicenseKeyDescription")}</p>
-            {isOnline ? (
-              <div>
-                <div className="mt-xxsmall">
-                  <SectionTitle title={t("module:licenseKey")} />
-                  <div className="width-100">
-                    <textarea
-                      className="textarea"
-                      rows={1}
-                      value={licenseKey}
-                      onChange={(e) => setLicenseKey(e.target.value)}
-                      placeholder={t("license:enterYourLicenseKeyHere")}
-                    />
-                    {response && (
-                      <div>
-                        <pre className="note success">
-                          {t("license:activateSuccess")}
-                        </pre>
-                      </div>
-                    )}
-                    {error && (
-                      <div>
-                        <pre className="note error">{error}</pre>
-                      </div>
-                    )}
-                  </div>
-                </div>
-                {!response && (
-                  <div className="mt-xsmall">
-                    <FigmaButton
-                      title={t("license:proceed")}
-                      onClick={handleVerify}
-                      disabled={loading}
-                    />
-                  </div>
-                )}
-              </div>
-            ) : (
-              <div>
-                <p>{t("license:offlineNotification")}</p>
-              </div>
-            )}
-            <div>
-              {!response ? (
-                licenseManagement.tier != "PAID" ? null : null
-              ) : (
-                <FigmaButton
-                  title={t("license:finish")}
-                  onClick={handleCloseCTSubscribe}
-                />
-              )}
-            </div>
-          </div>
-        )} */}
-        {!showActivateModal && freeTrialViewHandler()}
-      </div>
+      {freeTrialViewHandler()}
     </Modal>
   );
 };
