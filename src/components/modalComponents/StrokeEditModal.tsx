d0857e5852c3611bad9234ee1b87768a4712687a
chengyi <johnny.hsieh.12@gmail.com>
2025-03-19 13:47:48 +0800

    畫箭頭：製作儲存筆畫樣式功能

--- src/components/modalComponents/StrokeEditModal.tsx
+++ src/components/modalComponents/StrokeEditModal.tsx
@@ -4,10 +4,10 @@ import { useTranslation } from "react-i18next";
 import Modal from "../Modal";
 import FigmaButton from "../FigmaButton";
 import SectionTitle from "../SectionTitle";
-import { checkProFeatureAccessibleForUser } from "../../module-frontend/utilFrontEnd";
-import { MessageUnifyText } from "../../types/Messages/MessageShortcut";
+import { checkProFeatureAccessibleForUser, generateUUID } from "../../module-frontend/utilFrontEnd";
 import StrokeEditorView from "../StrokeEditorView";
 import { CYStroke } from "../../types/CYStroke";
+import { MessageSaveEditorPreference } from "../../types/Messages/MessageSaveEditorPreference";
 
 interface StrokeEditModalProps {
   show: boolean;
@@ -24,42 +24,47 @@ const StrokeEditModal: React.FC<StrokeEditModalProps> = ({
 }) => {
 
   const { t } = useTranslation(["module"]);
-  const { licenseManagement, setFreeUserDelayModalConfig } = useAppContext();
+  const {
+    licenseManagement,
+    setFreeUserDelayModalConfig,
+    editorPreference,
+    setEditorPreference
+  } = useAppContext();
 
-  const [targetTextContent, setTargetTextContent] = useState("");
-  const handleTargetTextContentChange = (
+  const [styleName, setStyleName] = useState("New stroke style");
+  const handleStyleNameChange = (
     event: React.ChangeEvent<HTMLTextAreaElement>
   ) => {
-    setTargetTextContent(event.target.value);
+    setStyleName(event.target.value);
   };
 
-  const unifyText = (isRealCall = false) => {
+  const saveStyle = (isRealCall = false) => {
     if (!isRealCall) {
       if (!checkProFeatureAccessibleForUser(licenseManagement)) {
         setFreeUserDelayModalConfig({
           show: true,
           initialTime: 30, // Adjust the delay time as needed
-          onProceed: () => unifyText(true), // Retry with `isRealCall = true`
+          onProceed: () => saveStyle(true), // Retry with `isRealCall = true`
         });
         return;
       }
     }
 
-    const message: MessageUnifyText = {
-      module: "Shortcut",
-      direction: "Inner",
-      phase: "Actual",
-      targetTextContent: targetTextContent,
-      action: "unifyText",
-    };
+    setEditorPreference((prev) => ({
+      ...prev,
+      strokeStyles: [...prev.strokeStyles, { name: styleName, id: generateUUID(), style: stroke }]
+    }));
 
-    parent.postMessage(
-      {
-        pluginMessage: message,
-      },
-      "*"
-    );
-  };
+    const message: MessageSaveEditorPreference = {
+      editorPreference: editorPreference,
+      shouldSaveEditorPreference: true,
+      module: "General",
+      phase: "Actual"
+    }
+
+    parent.postMessage({ pluginMessage: message, }, "*");
+  }
+
 
   return (
     <Modal show={show} handleClose={handleClose}>
@@ -67,12 +72,24 @@ const StrokeEditModal: React.FC<StrokeEditModalProps> = ({
         <h3>New stroke style</h3>
       </div>
       <div className="mt-xsmall">
+        <div>
+          <SectionTitle title={t("term:name")} titleType="secondary" />
+          <div className="width-100">
+            <textarea
+              className="textarea textarea-height-fit-content"
+              rows={1}
+              value={styleName}
+              onChange={handleStyleNameChange}
+            />
+          </div>
+        </div>
         <StrokeEditorView stroke={stroke} setStroke={setStroke} />
       </div>
       <div className="mt-xsmall">
         <FigmaButton
           title={t("module:save")}
           onClick={() => {
+            saveStyle();
             handleClose();
           }} />
       </div>
--- src/types/Module.ts
+++ src/types/Module.ts
@@ -1,5 +1,6 @@
 export type Module =
   | "Init"
+  | "General"
   | "Localization"
   | "Spaciiing"
   | "Shortcut"
