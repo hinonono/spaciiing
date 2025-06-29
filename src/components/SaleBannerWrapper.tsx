cd57e746f63e96aae5d6a45fa4a0da39e8218cb1
chengyi <johnny.hsieh.12@gmail.com>
2025-06-29 11:50:29 +0800

    版本32：整體優化

--- src/components/SaleBannerWrapper.tsx
+++ src/components/SaleBannerWrapper.tsx
@@ -14,30 +14,13 @@ const SaleBannerWrapper: React.FC<SaleBannerWrapperProps> = ({
 }) => {
   const { isVerifying } = useAppContext();
 
-  const [shouldShowBanner, setShouldShowBanner] = useState<
-    "NONE" | "SALE" | "NORMAL"
-  >("NONE");
-
-  useEffect(() => {
-    if (licenseManagement.tier !== "PAID") {
-      setShouldShowBanner("NORMAL");
-    } else {
-      // 當用戶的狀態是付費用戶時，直接不顯示Banner
-      setShouldShowBanner("NONE");
-    }
-  }, [licenseManagement]);
-
-  useEffect(() => { }, [isVerifying])
-
-  if (shouldShowBanner === "NONE") {
+  if (licenseManagement.tier === "PAID") {
     return null;
-  } else if (shouldShowBanner === "NORMAL") {
-    if (isVerifying) {
-      return <div className="banner flex flex-justify-center align-items-center"><Spinner /></div>
-    } else {
-      return <div className="banner flex flex-justify-space-between align-items-center"><NormalBanner /></div>;
-    }
   }
+
+  return (
+    isVerifying ? <div className="banner flex flex-justify-center align-items-center"><Spinner /></div> : <div className="banner flex flex-justify-space-between align-items-center"><NormalBanner /></div>
+  );
 };
 
 export default SaleBannerWrapper;
