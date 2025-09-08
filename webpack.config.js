74835b8332f490addc04c45e4ba1ab0ae788a1aa
chengyi <johnny.hsieh.12@gmail.com>
2025-09-08 11:14:15 +0800

    Fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
-const webpack = require("webpack");
-const dotenv = require("dotenv");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const HtmlInlineScriptPlugin = require("html-inline-script-webpack-plugin").default;
-const HtmlInlineCSSPlugin = require("html-inline-css-webpack-plugin").default;
+const webpack = require("webpack");
+const dotenv = require("dotenv");
 const TerserPlugin = require("terser-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
@@ -37,37 +34,22 @@ module.exports = (env, argv) => {
         },
         {
           test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, "css-loader"],
+          use: ["style-loader", "css-loader"],
         },
       ],
     },
     plugins: [
-      new MiniCssExtractPlugin({
-        filename: "style.css", // This CSS will be inlined
-      }),
       new HtmlWebpackPlugin({
         template: "./src/index.html",
         filename: "index.html",
-        inject: "body",
         chunks: ["bundle"], // Only include the 'bundle' entry in the HTML
-        minify: isProduction
-          ? {
-              collapseWhitespace: true,
-              removeComments: true,
-            }
-          : false,
-        scriptLoading: "blocking",
       }),
-      // new HtmlInlineScriptPlugin({
-      //   scriptMatchPattern: [/bundle.js$/], // Inline only 'bundle.js'
-      // }),
       new webpack.DefinePlugin({
         "process.env.REACT_APP_ENV": JSON.stringify(process.env.REACT_APP_ENV),
       }),
       new CopyWebpackPlugin({
         patterns: [{ from: "public/locales", to: "locales" }],
       }),
-      new HtmlInlineCSSPlugin(), // ✅ Inlines styles.css into index.html
     ],
     optimization: {
           minimize: true,
