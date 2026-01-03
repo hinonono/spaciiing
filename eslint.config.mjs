9024ee08f540c095f2afb3e555b88daf7afd3165
chengyi <johnny.hsieh.12@gmail.com>
2026-01-03 16:53:48 +0800

    更新依賴程式庫

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import * as figmaPlugin from '@figma/eslint-plugin-figma-plugins';
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig(
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@figma/figma-plugins': figmaPlugin,
+    },
+    rules: {
+      ...figmaPlugin.flatConfigs.recommended.rules,
+    },
+  }
+);
